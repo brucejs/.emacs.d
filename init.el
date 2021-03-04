8e5f0d75a618d4895b45c6199eba019610b9528b
Bruce Stephenson <bruce@brucejs.com>
2021-03-03 18:52:10 -0600

    Customize deft front matter

--- init.el
+++ init.el
@@ -38,3 +38,25 @@
     (message (concat "\"" (buffer-name) "\" clipped!")))
 
 (global-set-key [f6] 'brucejs/clip-buffer-name)
+
+;; override deft-auto-populate-title-maybe function so that #+TITLE: and
+;; #+FILETAGS: frontmatter are inserted automatically
+(defun deft-auto-populate-title-maybe (file)
+  "Possibly populate title line for FILE using filter string.
+If the filter string is non-nil and `deft-use-filename-as-title'
+is nil, then use the filter string to populate the title line in
+the newly created FILE."
+  (when (and deft-filter-regexp (not deft-use-filename-as-title))
+    (write-region
+     (concat
+      (cond
+       ((and (> deft-markdown-mode-title-level 0)
+             (string-match "^\\(txt\\|text\\|md\\|mdown\\|markdown\\)"
+                           deft-default-extension))
+        (concat (make-string deft-markdown-mode-title-level ?#) " "))
+       ((and deft-org-mode-title-prefix
+             (string-equal deft-default-extension "org"))
+        "#+TITLE: "))
+      (deft-whole-filter-regexp)
+      "\n#+FILETAGS: \n\n")
+     nil file nil)))
