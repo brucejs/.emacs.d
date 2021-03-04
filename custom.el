3d95a941e5f5e4cab15292bbef3231c5b627cd05
Bruce Stephenson <bruce@brucejs.com>
2021-03-03 19:01:12 -0600

    Change diary-file

--- custom.el
+++ custom.el
@@ -10,7 +10,7 @@
  '(deft-directory (concat org-directory "/GTD"))
  '(deft-new-file-format "%Y%m%dT%H%M%SZ")
  '(deft-org-mode-title-prefix t)
- '(diary-file "~/Documents/Notes/GTD/diary.org")
+ '(diary-file "~/Documents/Notes/GTD/20210304T005429Z.org")
  '(fill-column 80)
  '(global-display-line-numbers-mode t)
  '(indent-tabs-mode nil)
